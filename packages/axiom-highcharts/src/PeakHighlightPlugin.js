23f23ef604f2a14ade97c4a4aad322d53f873ce6
Stefan Buck <stefanb@brandwatch.com>
2018-09-18 14:59:23 +0200

    Refactoring

--- packages/axiom-highcharts/src/PeakHighlightPlugin.js
+++ packages/axiom-highcharts/src/PeakHighlightPlugin.js
@@ -1,51 +1,78 @@
 
-function createGroup(chart, index) {
-  const group = chart.renderer.g('peak-group-' + index);
-  group.add();
-  return group;
+class PeakHighlight {
+  constructor(chart, peakCoordinates) {
+    const path = peakCoordinates.map(({ x, y }, index) => (index > 0 ? 'L' : 'M') + ` ${x} ${y}`);
+
+    return chart.renderer
+    .path((path))
+    .attr({
+      fill: 'red',
+    });
+  }
 }
 
-function findPeakCoordinates(chart) {
-  const data = chart.yAxis[0].series[0].data;
+class PeakHighlightGroup {
+  constructor(chart) {
+    this.chart = chart;
 
-  return chart.peaks.data.map(peak => {
-    const inner = data.filter(({ x }) => peak.includes(x));
-    inner.unshift({ plotX: inner[0].plotX, plotY: chart.clipBox.height });
-    inner.push({ plotX: inner[inner.length - 1].plotX, plotY: chart.clipBox.height });
-    return inner;
-  });
+    const group = this.createGroup(0);
+    const peakPaths = this.findPeakCoordinates();
+
+    peakPaths.map(peakArea => {
+      const svgElement = new PeakHighlight(this.chart, peakArea);
+      svgElement.add(group);
+    });
+
+    return group;
+  }
+
+  createGroup(index) {
+    const group = this.chart.renderer.g('peak-group-' + index);
+    group.translate(this.chart.plotLeft, this.chart.plotTop);
+    group.add();
+    return group;
+  }
+
+  findPeakCoordinates() {
+    const data = this.chart.yAxis[0].series[0].data;
+
+    return this.chart.peaks.data.map(peak => {
+      const coordinates = data.filter(({ x }) => peak.includes(x)).map(item => ({
+        x: item.plotX,
+        y: item.plotY,
+      }));
+
+      // Left bottom
+      coordinates.unshift({
+        x: coordinates[0].x,
+        y: this.chart.clipBox.height,
+      });
+
+      // Right bottom
+      coordinates.push({
+        x: coordinates[coordinates.length - 1].x,
+        y: this.chart.clipBox.height,
+      });
+
+      return coordinates;
+    });
+  }
 }
+
+
+
 
 function drawPeakAreas(chart) {
 
   if (chart.peaks._groups) {
-    chart.peaks._groups.forEach(svgElement => {
-      svgElement.destroy();
-      svgElement = null;
+    chart.peaks._groups.forEach(peakHighlightGroup => {
+      peakHighlightGroup.destroy();
+      peakHighlightGroup = null;
     });
   }
 
-  const peakPaths = findPeakCoordinates(chart);
-
-  const group = createGroup(chart, 0);
-
-  chart.peaks._groups = peakPaths.map(peakArea => {
-    const path = peakArea.map(({ plotX, plotY }, index) => (index > 0 ? 'L' : 'M') + ` ${plotX} ${plotY}`);
-    const svgElement = chart.renderer
-      .path((path))
-      .attr({
-        fill: 'green',
-      })
-      .translate(chart.plotLeft, chart.plotTop);
-
-    svgElement.add(group);
-
-    return svgElement;
-  });
-
+  chart.peaks._groups = [new PeakHighlightGroup(chart)];
 }
-
-
 
 export default (H) => {
 
