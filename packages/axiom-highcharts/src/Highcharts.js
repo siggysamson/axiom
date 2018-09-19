d28c5bd6d8b7160e6d310f36fde891915a384886
Thomas Sauer <t-sauer@outlook.de>
2018-09-19 14:40:01 +0200

    --wip-- [skip ci]

--- packages/axiom-highcharts/src/Highcharts.js
+++ packages/axiom-highcharts/src/Highcharts.js
@@ -1,6 +1,9 @@
 import './styles/axiom-highcharts.css';
 import base from './base';
 import Highcharts from 'highcharts/js/highcharts';
+import AnnotationsModule from 'highcharts/js/modules/annotations.src.js';
+
+AnnotationsModule(Highcharts);
 
 if (Highcharts.setOptions && typeof Highcharts.setOptions === 'function') {
   Highcharts.setOptions(base);
--- site/components/Documentation/Resources/Highcharts/Chart.js
+++ site/components/Documentation/Resources/Highcharts/Chart.js
@@ -15,47 +15,47 @@ export default class AxiomHighchartsLineChart extends React.Component {
   }
 
   componentDidMount() {
-    let self = this;
+    const self = this;
 
-    this.ref.current.container.addEventListener('click', function (e) {
-      let chart = self.ref.current.chart;
-      let point = chart.getSelectedPoints()[0];
-      let newState = {};
+    // this.ref.current.container.addEventListener('click', function(e) {
+    //   const chart = self.ref.current.chart;
+    //   const point = chart.getSelectedPoints()[0];
+    //   let newState = {};
 
-      if (!self.state.inSelectMode && e.target.classList.contains('highcharts-point-select')) {
-        let left = chart.plotLeft + point.plotX + 30;
-        let top = chart.plotTop + point.plotY - 20;
+    //   if (!self.state.inSelectMode && e.target.classList.contains('highcharts-point-select')) {
+    //     const left = chart.plotLeft + point.plotX + 30;
+    //     const top = chart.plotTop + point.plotY - 20;
 
-        if (point.graphic) {
-          newState = {
-            inSelectMode: true,
-            selectedItem: point,
-            left,
-            top
-          };
-        }
-      } else {
-        newState = {
-          inSelectMode: false,
-          selectedItem: null,
-        };
-      }
+    //     if (point.graphic) {
+    //       newState = {
+    //         inSelectMode: true,
+    //         selectedItem: point,
+    //         left,
+    //         top,
+    //       };
+    //     }
+    //   } else {
+    //     newState = {
+    //       inSelectMode: false,
+    //       selectedItem: null,
+    //     };
+    //   }
 
-      self.setState(newState, () => {
-        let options = {
-          plotOptions: {
-            series: {
-              enableMouseTracking: !newState.inSelectMode
-            },
-          },
-          tooltip: {
-            enabled: !newState.inSelectMode
-          }
-        };
+    //   self.setState(newState, () => {
+    //     const options = {
+    //       plotOptions: {
+    //         series: {
+    //           enableMouseTracking: !newState.inSelectMode,
+    //         },
+    //       },
+    //       tooltip: {
+    //         enabled: !newState.inSelectMode,
+    //       },
+    //     };
 
-        chart.update(options);
-      });
-    });
+    //     chart.update(options);
+    //   });
+    // });
   }
 
   componentDidUpdate() {
@@ -78,29 +78,33 @@ export default class AxiomHighchartsLineChart extends React.Component {
     return (
       <div>
         <HighchartsReact
-          ref={this.ref}
-          highcharts={Highcharts}
-          options={{
-            chart: {
-              type: 'line',
-              height: 400,
-              zoomType: 'x',
-            },
-            xAxis: {
-              type: 'datetime',
-            },
-            yAxis: {
-              title: {
-                text: 'Volume of Total Mentions Daily'
+            highcharts={ Highcharts }
+            options={ {
+              chart: {
+                type: 'line',
+                height: 400,
+                zoomType: 'x',
+                events: {
+                  click: (a, b) => {
+                    console.log('click', a, b);
+                  },
+                },
               },
-              tickInterval: 10,
-              endOnTick: true
-            },
-            plotOptions: {
-              series: {
-                pointStart: Date.UTC(2017, 11, 1),
-                pointInterval: 24 * 36e5,
-                allowPointSelect: true,
+              xAxis: {
+                type: 'datetime',
+              },
+              yAxis: {
+                title: {
+                  text: 'Volume of Total Mentions Daily',
+                },
+                tickInterval: 10,
+                endOnTick: true,
+              },
+              plotOptions: {
+                series: {
+                  pointStart: Date.UTC(2017, 11, 1),
+                  pointInterval: 24 * 36e5,
+                  allowPointSelect: true,
                 // point: {
                 //   events: {
                 //     select: function () {
@@ -108,37 +112,50 @@ export default class AxiomHighchartsLineChart extends React.Component {
                 //     }
                 //   }
                 // }
-              }
-            },
-            tooltip: {
-              outside: true
-            },
-            series: [{
-              name: 'Series 1',
-              data: [7, 7, 14, 7, 8, 7, 6, 9, 15, 11, 12, 8, 2, 7, 16, 9, 17, 18, 23, 17, 21, 12, 13, 14, 15, 18, 14],
-            }, {
-              name: 'Series 2',
-              data: [14, 18, 15, 14, 13, 12, 21, 17, 23, 18, 17, 9, 16, 7, 2, 8, 12, 11, 15, 9, 6, 7, 8, 7, 14, 7, 7],
-            }, {
-              colorIndex: 'blast-off',
-              name: 'Series 3',
-              data: [23, 28, 12, 18, 20, 22, 21, 37, 13, 8, 7, 13, 21, 18, 23, 11, 20, 16, 10, 15, 12, 17, 18, 37, 12, 9, 14],
-            }]
-          }}
-        />
+                },
+              },
+              tooltip: {
+                outside: true,
+              },
+              series: [{
+                name: 'Series 1',
+                data: [7, 7, 14, 7, 8, 7, 6, 9, 15, 11, 12, 8, 2, 7, 16, 9, 17, 18, 23, 17, 21, 12, 13, 14, 15, 18, 14],
+              }, {
+                name: 'Series 2',
+                data: [14, 18, 15, 14, 13, 12, 21, 17, 23, 18, 17, 9, 16, 7, 2, 8, 12, 11, 15, 9, 6, 7, 8, 7, 14, 7, 7],
+              }, {
+                colorIndex: 'blast-off',
+                name: 'Series 3',
+                data: [23, 28, 12, 18, 20, 22, 21, 37, 13, 8, 7, 13, 21, 18, 23, 11, 20, 16, 10, 15, 12, 17, 18, 37, 12, 9, 14],
+              }],
+              annotations: [{
+                labels: [{
+                  point: {
+                    x: Date.UTC(2017, 11, 1),
+                    y: 7,
+                    xAxis: 0,
+                    yAxis: 0,
+                  },
+                  text: 'A',
+                  shape: 'circle',
+                  className: 'highcharts-color-0 highcharts-tooltip',
+                }],
+              }],
+            } }
+            ref={ this.ref }/>
         {
         //  better to use popper.js here
         }
         {
           this.state.inSelectMode ?
             (
-              <div style={{
+              <div style={ {
                 position: 'absolute',
                 left: this.state.left - (232 / 2),
                 top: this.state.top,
-              }}>
-                <Context position='bottom'>
-                  <Heading textSize="large">{this.state.selectedItem.series.name}: {this.state.selectedItem.y}</Heading>
+              } }>
+                <Context position="bottom">
+                  <Heading textSize="large">{ this.state.selectedItem.series.name }: { this.state.selectedItem.y }</Heading>
                   <ul>
                     <li>Menu Item</li>
                     <li>Menu Item</li>
